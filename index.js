95927d221daba6156058636038cf0585507f58c1
Miguel Angel Bernal Valdez <dm.mbernal@gmail.com>
2020-03-31 16:06:09 -0400

    Sub páginas

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const express = require('express')
 // Se crea la variable u objeto de express
 const app = express()
 // Variables de la aplicacion
-app.set('appName','Sitio del Colegio')
-app.set('port',3000)
+app.set('appName','Colegio Nacional Gral. Elizardo Aquino')
+app.set('port', process.env.PORT || 3000)
 app.set('view engine','ejs')
 // Configurara la carpeta de static
 app.use(express.static('static'))
 // Ruta principal
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    var title = app.get('appName')
+    res.render('index.ejs', {title: title} )
+})
+app.get('/electronica', (req, res) => {
+    var title = app.get('appName')+" | Electrónica"
+    res.render('electronica.ejs', {title: title} )
 })
 // Levanta el servidor
 app.listen(app.get('port'), () => {
